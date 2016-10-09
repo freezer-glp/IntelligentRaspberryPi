e2a3182af4a8926eb7684af454820623c53f0ad8
freezer-glp <409349942@qq.com>
2016-10-09 17:43:24 +0800

    change file name

--- PiFan/FanScheduler.py
+++ PiFan/FanScheduler.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import os
+import time
 import FanController
 import traceback
 
 StartFanTemp = 45
 StopFanTemp = 34
-
+ISOTIMEFORMAT="[%Y-%m-%d %X]"
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     return float(res.replace("temp=","").replace("'C\n",""))
@@ -13,7 +14,7 @@ def getCPUtemperature():
 def fanScheduler():
     try:
         temp = int(round(getCPUtemperature()))
-        print "CPU temp:" + str(temp)
+        print  time.strftime( ISOTIMEFORMAT)+" CPU temp:" + str(temp) 
         if temp >= StartFanTemp :
             FanController.startFan()
             print "Start fan."
